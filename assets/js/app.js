1153b5fe2fef7af887a7e322646bd0e93ff083df
Nicolas <nicolas35hermange@gmail.com>
2019-07-04 09:45:07 +0200

    Modify => Only if app.user = event.user

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-import 'tinymce/themes/modern/theme';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -20,10 +20,9 @@ console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 $(document).ready(function() {
 
-    tinymce.init({
-        selector: 'textarea'
-    });
-
+   // tinymce.init({
+    // selector: 'textarea'
+   //});
     $('select').select2();
 
     $('[data-toggle="tooltip"]').tooltip();
